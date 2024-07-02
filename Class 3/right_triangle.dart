5919617dc77a1adf921ef6e172cab074ce24e35e
Abdun Nafi <abdunnafi@Abduns-MacBook-Air.local>
2024-07-02 14:28:07 +0600

    ddd

---
+++ Class 3/right_triangle.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+void main() {
+  for (var i = 1; i <= 5; i++) {
+    for (var j = 1; j <= i; j++) {
+      stdout.write('*');
+      
+    }
+    print("");
+  }
+  for (var i = 1; i <= 4; i++) {
+    for (var j = 1; j<= i+3 ; j++) {
+      stdout.write('*');
+      
+    }
+    print("");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'dart:io';
+
+// void printPyramid(int n) {
+//   for (int i = 0; i < n; i++) {
+//     // Print leading spaces
+//     for (int j = 0; j < n - i - 1; j++) {
+//       stdout.write(' ');
+//     }
+//     // Print stars
+//     for (int k = 0; k < 2 * i + 1; k++) {
+//       stdout.write('*');
+//     }
+//     // Move to the next line
+//     print('');
+//   }
+// }
+
+// void main() {
+//   printPyramid(5);
+// }
